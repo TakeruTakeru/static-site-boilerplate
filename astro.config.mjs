a0d191949eec4845e49fccbbcbd354a9aeb82779
TakeruTakeru <vitual.money.tkr@gmail.com>
2025-07-11 14:57:18 +0900

    astro.config.mjs: Add sitemap and react integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  outDir: "./dist",
+  output: 'static',
+  outDir: './dist',
+  integrations: [sitemap(), react()],
   vite: {
     plugins: [tailwindcss()],
   },
 });
+
+
